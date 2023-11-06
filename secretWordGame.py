c44636744bcd9dbfc1ef344432905d0bd1081eca
Leonardo Lira de Amorim <leonardolira@gmail.com>
2023-11-06 17:32:57 -0300

    Add files via upload

---
+++ secretWordGame.py
@@ -0,0 +1,20 @@
+SECRETWORD = 'perfume'
+encryptedWord = len(SECRETWORD) * '*'
+attempts = 0
+while encryptedWord!=SECRETWORD:
+    print(f'\nSecret word is: {encryptedWord}')
+    inputLetter = input('\nType a letter for the secret word.\n')
+    modSecretWord = SECRETWORD
+    if len(inputLetter) > 1:
+        print('\nType one letter only for each input!')
+    elif inputLetter in modSecretWord:
+        attempts = attempts+1
+        for letter in modSecretWord:
+            if letter == inputLetter:
+                letterIndex = modSecretWord.find(letter)
+                replacementLetter = modSecretWord[letterIndex]
+                modSecretWord = modSecretWord[0:letterIndex]+ '*' + modSecretWord[letterIndex+1:]
+                encryptedWord = encryptedWord[0:letterIndex]+ replacementLetter + encryptedWord[letterIndex+1:] 
+
+print(f'You won! The secret word is: {encryptedWord}')
+print(f'Total amount of attempts: {attempts}!')
